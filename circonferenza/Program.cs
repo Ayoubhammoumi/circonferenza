a6fc9f52a28e41144e1a78b2bad20e1c7604c8a9
39320 <39320@DESKTOP-IJMGSHS>
2024-01-25 19:03:24 +0100

    Aggiungere i file di progetto.

---
+++ circonferenza/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circonferenza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            circonferenza circonferenza = new circonferenza();
+
+            circonferenza.raggio = 5;
+
+            Console.WriteLine("{0}", circonferenza.calcolaperimetro(circonferenza.perimetro));
+
+            Console.WriteLine("{0}", circonferenza.calcoloarea(circonferenza.area));
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ circonferenza/circonferenza.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circonferenza
+{
+    internal class circonferenza
+    {
+        public float raggio { get; set; }
+
+       public float perimetro { get; set; }
+
+        public float area { get; set; }
+        public float calcolaperimetro (float perimetro )
+        {
+            perimetro = (float)(raggio * Math.PI * 2);
+
+            return perimetro;
+        }
+
+        public float calcoloarea (float area)
+        {
+            area = (float)(raggio * Math.PI * Math.PI);
+
+            return area;
+        }
+    }
+}
